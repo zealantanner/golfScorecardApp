972838d96bf1706e1dae6c6849324eb43df4a287
zealantanner <zealantanner@gmail.com>
2022-03-18 07:46:29 -0600

    init

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function getOneActivityPromise() {
+    return fetch("https://www.boredapi.com/api/Activity")
+    .then(response => response.json())
+    .then(data => activities.push(data));
+}
+
+let activities = []
+async function getMultipleActivities(times) {
+    activities = []
+    for(let i=0;i<times;i++) {
+        const activity = getOneActivityPromise()
+        activities.push(await activity)
+    }
+    return activities
+}
+
+async function render(input) {
+    activities = getMultipleActivities(input)
+    console.log(activities)
+    let activitiesHtml = 'hi'
+    for(let i = 0; i < activities.length; i++) {
+        activitiesHtml += `<div id="${activities[i].activity}">${activities[i].activity}</div>`;
+    }
+
+    document.querySelector('#activities').innerHTML = activitiesHtml;
+}
+
+
+
+function displayActivities(list) {
+    let listHtml = 'hi'
+    for(let i = 0; i < list.length; i++) {
+        listHtml += `<div id="${list[i].name}">${list[i].name}</div>`;
+    }
+
+    document.querySelector('#activities').innerHTML = listHtml;
+}
